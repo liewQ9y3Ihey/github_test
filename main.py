1fe7354f79b47f2d104e42280c53e64c7159583f
Robert <rwinter@aptations.com>
2026-02-23 09:24:00 -0800

    Change add values

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utils.math_tools import add, multiply, subtract
 from utils.file_helpers import save_text
 
 def main():
-    print("Add:", add(5, 3))
+    print("Add:", add(10, 20))
     print("Multiply:", multiply(4, 6))
     print("Subtract:", subtract(10, 4))
     save_text("output.txt", "This is a test.")
